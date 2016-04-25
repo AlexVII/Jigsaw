3f235cbdc9e70e469eb06ae96a729fd10b1e3a3d
bme-lab2 <bme@lab2.com>
2016-04-26 03:28:49 +0800

    Added Jigsaw Activity.

---
+++ app/src/main/java/com/smu_bme/jigsaw/JigsawActivity.java
@@ -0,0 +1,24 @@
+package com.smu_bme.jigsaw;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class JigsawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jisaw);
+        Button button = (Button) findViewById(R.id.get_started);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JigsawActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
