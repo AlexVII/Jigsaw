e59a258e0802e57494c6ef1e8a26106aaa803749
bme-lab <bme-lab@smu.com>
2016-04-26 23:15:13 +0800

    Figure

---
+++ app/src/main/java/com/smu_bme/jigsaw/StatisticFigure.java
@@ -0,0 +1,28 @@
+package com.smu_bme.jigsaw;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+
+public class StatisticFigure extends AppCompatActivity {
+////////////////////////////////////////////////
+
+    //BarChart
+    BarChart week = (BarChart) findViewById(R.id.weekChart);
+//    BarChart        new BarChart(StatisticFigure.this);
+
+
+
+////////////////////////////////////////////////
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistic_figure);
+    }
+
+
+}
+
