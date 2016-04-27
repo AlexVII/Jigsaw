020782ddf5ff57139fd5e72f6afa2e169c94c667
BME_LAB4 <64287644278368721643@qq.com>
2016-04-27 23:30:31 +0800

    DB

--- app/src/main/java/com/smu_bme/jigsaw/JigsawActivity.java
+++ app/src/main/java/com/smu_bme/jigsaw/JigsawActivity.java
@@ -5,12 +5,17 @@ package com.smu_bme.jigsaw;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JigsawActivity extends AppCompatActivity {
 
@@ -28,59 +33,85 @@ public class JigsawActivity extends AppCompatActivity {
             }
         });
     }
+
+public class DbData {
+    int id;
+    String date;
+    String duration;
+    String time;
+    String name;
+    String remaek;
 }
+    public class MydatabaseHelper extends SQLiteOpenHelper {
+        private String correntName;
+        private Context context;
+        public static final String CREATE_DATA = "create table DATA ("
+                + "id integer primary key autoincrement, "
+                + "date text"
+                + "duration integer"
+                + "time text"
+                + "name text"
+                + "remark text";
 
-public class MydatabaseHelper extends SQLiteOpenHelper {
-    public static final String CREATE_DATA = "create table DATA ("
-            + "id integer primary key autoincrement, "
-            + "date text"
-            + "duration integer"
-            + "time text"
-            + "name text"
-            + "remark text";
+        public static final String CREATE_SUM = "create table SUM("
+                + "id integer primary key autoincrement,"
+                + "date text"
+                + "sum integer)";
 
-    public static final String CREATE_SUM = "create table SUM("
-            + "id integer primary key autoincrement,"
-            +"date text"
-            +"sum integer)";
 
-    private Context mContext;
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_DATA);
+            db.execSQL(CREATE_SUM);
+            db.execSQL("insert into SUM (date,sum) values(?,?)", new String[]{"all", "0"});
+        }
 
-    public MydatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
-        super(context, name, factory, version);
-        mContext = context;
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        }
+
+        MydatabaseHelper db_one = new MydatabaseHelper();
+
+        public List<DbData> findData(String mode,String item) {
+            SQLiteDatabase db = db_one.getWritableDatabase();
+            List<DbData> list = new ArrayList<DbData>();
+
+            Cursor cursor = db.rawQuery("select * from DATA where ?=?",new String[]{ mode ,item});
+            while (cursor.moveToFirst()) {
+                DbData dbData = new DbData();
+                dbData.id = cursor.getInt(cursor.getColumnIndex("id"));
+                dbData.date = cursor.getString(cursor.getColumnIndex("data"));
+                dbData.duration = cursor.getString(cursor.getColumnIndex("duration"));
+                dbData.time = cursor.getString(cursor.getColumnIndex("time"));
+                dbData.name = cursor.getString(cursor.getColumnIndex("name"));
+                dbData.remaek = cursor.getString((cursor.getColumnIndex("remark")));
+                list.add(dbData);
+            }
+            cursor.close();
+            db.close();
+            return list;
+        }
+
+        public void addData(String name, String date, String time, String duration, String remark) {
+
+            SQLiteDatabase db = db_one.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            //开始组装数据
+            values.put("name", name);
+            values.put("date", date);
+            values.put("time", time);
+            values.put("duration", duration);
+            values.put("remark", remark);
+            db.insert("DATA", null, values);
+            values.clear();
+        }
+
+
+        public void deleteData(String id) {
+            SQLiteDatabase db = db_one.getWritableDatabase();
+            db.execSQL("delete from DATA where id=? ", new String[]{id});
+        }
+
+
     }
-
-    @Override
-    public void onCreate(SQLiteDatabase db){
-        db.execSQL(CREATE_DATA);
-        db.execSQL(CREATE_SUM);
-        db.execSQL("insert into SUM (date,sum) values(?,?)",new String[] {"all","0"});
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-    }
-
-    public void addData(String name,String date,String time,String duration, String remark){
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        //开始组装数据
-        values.put("name",name);
-        values.put("date",date);
-        values.put("time",time);
-        values.put("duration",duration);
-        values.put("remark",remark);
-        db.insert("DATA",null,values);
-        values.clear();
-    }
-
-
-    public void deleteData(String id){
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.execSQL("delete from DATA where id=? ",new String[]{id});
-    }
-
-    public void uo
-
 }
