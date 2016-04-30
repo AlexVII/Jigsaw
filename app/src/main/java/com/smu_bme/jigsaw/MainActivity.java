a12205a754647029213e23c650a6d9076761ac4b
Golly Rui <guoli-lv@hotmail.com>
2016-04-30 12:03:44 +0800

    nothing

--- app/src/main/java/com/smu_bme/jigsaw/MainActivity.java
+++ app/src/main/java/com/smu_bme/jigsaw/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -45,8 +46,9 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.concurrent.ThreadFactory;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     public FloatingActionButton fab;
 
@@ -57,7 +59,7 @@ public class MainActivity extends AppCompatActivity{
     private ViewPager mViewPager;
 //    private CreateEventListener listener;
 
-    public static final String CurrentDateString =  new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
+    public static final String CurrentDateString = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
     public static final String ShowedDateString = CurrentDateString;
     public static Calendar ShowedDate = Calendar.getInstance();
 
@@ -70,7 +72,7 @@ public class MainActivity extends AppCompatActivity{
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         mViewPager = (ViewPager) findViewById(R.id.container);
 
@@ -94,8 +96,8 @@ public class MainActivity extends AppCompatActivity{
                                 String nameInput = name.getText().toString();
                                 String remarkInput = remark.getText().toString();
 
-                                if (nameInput.equals("")){
-                                    Toast.makeText(MainActivity.this,getString(R.string.noName), Toast.LENGTH_SHORT).show();
+                                if (nameInput.equals("")) {
+                                    Toast.makeText(MainActivity.this, getString(R.string.noName), Toast.LENGTH_SHORT).show();
                                 } else if (remarkInput.equals("")) {
                                     Toast.makeText(MainActivity.this, getString(R.string.noRemark), Toast.LENGTH_SHORT).show();
                                 } else {
@@ -106,19 +108,20 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
-}
+    }
 
     public static class PlaceholderFragment extends Fragment {
 
-        String CurrentCalendar =  MainActivity.CurrentDateString;
+        String CurrentCalendar = MainActivity.CurrentDateString;
         String ShowedCalendar = CurrentCalendar;
         Calendar calendar;
-        int CurrentMins  = Calendar.getInstance().get(Calendar.MINUTE);
+        int CurrentMins = Calendar.getInstance().get(Calendar.MINUTE);
         private List<DbData> list;
 
         private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {}
+        public PlaceholderFragment() {
+        }
 
 
         public static PlaceholderFragment newInstance(int sectionNumber) {
@@ -133,174 +136,95 @@ public class MainActivity extends AppCompatActivity{
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            if (getArguments().getInt(ARG_SECTION_NUMBER) == 1){
+            if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
                 return initCardAndProgressBar(inflater, container);
             } else {
-                return initChart(inflater, container);
+                Chart chart = new Chart(inflater,container,getActivity(),ShowedDate);
+                return chart.initChart();
             }
         }
 
-        public View initCardAndProgressBar(LayoutInflater inflater, final ViewGroup container){
+        public View initCardAndProgressBar(LayoutInflater inflater, final ViewGroup container) {
             View rootView = inflater.inflate(R.layout.layout_log, container, false);
             final TextView textView = (TextView) rootView.findViewById(R.id.date);
             textView.setText(ShowedCalendar);
             ImageButton imageButton = (ImageButton) rootView.findViewById(R.id.edit_date);
             imageButton.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View v) {
-                   DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() {
-                       @Override
-                       public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                           calendar.set(Calendar.YEAR, year);
-                           calendar.set(Calendar.MONTH, monthOfYear);
-                           calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                           textView.setText(new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime()));
-                       }
-                   };
-                   new DatePickerDialog(getActivity(), dateListener,
-                           calendar.get(Calendar.DAY_OF_MONTH),
-                           calendar.get(Calendar.MONTH),
-                           calendar.get(Calendar.DAY_OF_MONTH)).show();
-               }
-           });
+                @Override
+                public void onClick(View v) {
+                    DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            calendar.set(Calendar.YEAR, year);
+                            calendar.set(Calendar.MONTH, monthOfYear);
+                            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                            textView.setText(new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime()));
+                        }
+                    };
+                    new DatePickerDialog(getActivity(), dateListener,
+                            calendar.get(Calendar.DAY_OF_MONTH),
+                            calendar.get(Calendar.MONTH),
+                            calendar.get(Calendar.DAY_OF_MONTH)).show();
+                }
+            });
             ProgressBar progressBar = (ProgressBar) rootView.findViewById(R.id.progress);
             progressBar.setMax(14400);
-            if (ShowedCalendar == CurrentCalendar){
-            progressBar.setSecondaryProgress(CurrentMins);
-            } else { progressBar.setSecondaryProgress(1440);
-        }
+            if (ShowedCalendar == CurrentCalendar) {
+                progressBar.setSecondaryProgress(CurrentMins);
+            } else {
+                progressBar.setSecondaryProgress(1440);
+            }
 //            progressBar.setProgress();
             RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.event_list);
             recyclerView.setHasFixedSize(true);
             LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
             recyclerView.setLayoutManager(linearLayoutManager);
             DbHelper dbHelper = new DbHelper(getActivity());
-            list = dbHelper.queryData("date","1970-1-1");
-            mAdapter mAdapter = new mAdapter(list, getActivity()) ;
+            list = dbHelper.queryData("date", "1970-1-1");
+            mAdapter mAdapter = new mAdapter(list, getActivity());
             recyclerView.setAdapter(mAdapter);
             return rootView;
 
         }
 
 
-        public View initChart (LayoutInflater inflater, ViewGroup container){
+        public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-            View rootView = inflater.inflate(R.layout.layout_data, container, false);
-            BarChart barChart = (BarChart) rootView.findViewById(R.id.bar_chart);
-            ArrayList<String> xVals = new ArrayList<>();
-
-            xVals.add("星期日");xVals.add("星期一");xVals.add("星期二");xVals.add("星期三");xVals.add("星期四");xVals.add("星期五");xVals.add("星期六");
-
-            ArrayList<BarEntry> valsComp1 = new ArrayList<>();
-            ArrayList<BarEntry> valsComp2 = new ArrayList<>();
-
-            Calendar calendar= ShowedDate;
-            SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
-            DbHelper dbHelper = new DbHelper(getActivity());
-            for(int i=1;i<8;i++) {
-                calendar.set(Calendar.DAY_OF_WEEK, i);
-                String date = f.format(calendar.getTime());
-                float sum = (float)dbHelper.querySum(date);
-                if (sum<0)sum=0;
-                BarEntry Chart1Element = new BarEntry(87,i-1);
-                valsComp1.add(Chart1Element);
+            public SectionsPagerAdapter(FragmentManager fm) {
+                super(fm);
             }
-//            BarEntry c1e1 = new BarEntry(233f, 0);
-//            valsComp1.add(c1e1);
-//            BarEntry c1e2 = new BarEntry(2333f, 1);
-//            valsComp1.add(c1e2);
-//            BarEntry c2e1 = new BarEntry(233f, 0);
-//            valsComp1.add(c2e1);
-//            BarEntry c2e2 = new BarEntry(233f, 1);
-//            valsComp1.add(c2e2);
 
-            BarDataSet setc1 = new BarDataSet(valsComp1, null);
-            setc1.setAxisDependency(YAxis.AxisDependency.LEFT);
-            BarDataSet setc2 = new BarDataSet(valsComp2, "C2");
-            setc2.setAxisDependency(YAxis.AxisDependency.LEFT);
+            @Override
+            public Fragment getItem(int position) {
+                return PlaceholderFragment.newInstance(position + 1);
 
-            ArrayList<IBarDataSet> dataSet = new ArrayList<IBarDataSet>();
-            dataSet.add(setc1);
-            dataSet.add(setc2);
-            BarData data = new BarData(xVals, dataSet);
-            data.setGroupSpace(30f);
-            barChart.setData(data);
-            barChart.setHighlightPerTapEnabled(true);
-//            barChart.setDrawBarShadow(true);
-//            barChart.setMinimumWidth(60);
+            }
 
-            barChart.invalidate();
+            @Override
+            public int getCount() {
+                return 2;
+            }
 
-
-            barChart.setDescription("一周学习");  // set the description
-            setc1.setColors(ColorTemplate.COLORFUL_COLORS);
-            setc2.setColors(ColorTemplate.COLORFUL_COLORS);
-            barChart.animateY(5000);
-
-
-            PieChart pieChart = (PieChart) rootView.findViewById(R.id.pie_chart);
-            ArrayList<String> labels = new ArrayList<String>();
-            pieChart.setUsePercentValues(true);
-            pieChart.setExtraOffsets(5, 10, 5, 5);
-            pieChart.setDragDecelerationFrictionCoef(0.95f);
-           // pieChart.setCenterText(false);
-            pieChart.setRotationAngle(0);
-            // enable rotation of the chart by touch
-            pieChart.setRotationEnabled(true);
-/
-// /            pieChart.setBackgroundColor(Color.LTGRAY);
-//            pieChart.setBackgroundTintMode();
-            pieChart.setHighlightPerTapEnabled(false);
-
-            // add a selection listener
-            // mPieChart.setOnChartValueSelectedListener(this);
-/*
-            TreeMap<String, Float> data3 = new TreeMap<>();
-            data3.put("data1", 0.5f);
-            data3.put("data2", 0.3f);
-            data3.put("data3", 0.1f);
-            data3.put("data4", 0.1f);
-            pieChart.setData(data3);
-
-            // 设置动画
-            pieChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
-
-            // 设置显示的比例
-            Legend l = pieChart.getLegend();
-            l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
-            l.setXEntrySpace(7f);
-            l.setYEntrySpace(0f);
-            l.setYOffset(0f);
-
-*/
-            labels.add("Math");
-            labels.add("English");
-            labels.add("Physics");
-        //    ArrayList<PieEntry> valsComp1 = new ArrayList<>();
-            ArrayList<Entry> entries = new ArrayList<>();
-            entries.add(new Entry(4f, 0));
-            entries.add(new Entry(8f, 1));
-            entries.add(new Entry(6f, 2));
-
-            // 设置显示的比例
-            Legend l = pieChart.getLegend();
-            l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
-            l.setXEntrySpace(7f);
-            l.setYEntrySpace(0f);
-            l.setYOffset(0f);
-
-            pieChart.setCenterText("每日学习");
-            PieDataSet dataset2 = new PieDataSet(entries, "项目");
-            PieData data2 = new PieData(labels, dataset2);
-            pieChart.setData(data2);
-      //      PieChart.invalidate();
-            pieChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
-            dataset2.setColors(ColorTemplate.COLORFUL_COLORS);
-            pieChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
-            return rootView;
-
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position) {
+                    case 0:
+                        return getString(R.string.Log);
+                    case 1:
+                        return getString(R.string.Data);
+                }
+                return null;
+            }
         }
-        public void initEvent(){
+
+        public void showPopup(View view) {
+            PopupMenu popupMenu = new PopupMenu(this,view);
+            MenuInflater inflater = popupMenu.getMenuInflater();
+            inflater.inflate(R.menu.duration, popupMenu.getMenu());
+            popupMenu.show();
+        }
+
+        public void initEvent() {
             DbData dbData1 = new DbData("1970-1-1", "03:33", 200, "Test1");
             DbData dbData2 = new DbData("1970-1-1", "05:33", 200, "Test2");
             DbData dbData3 = new DbData("1970-1-1", "20:33", 200, "Test1");
@@ -317,42 +241,6 @@ public class MainActivity extends AppCompatActivity{
             dbHelper.addData(dbData6);
             dbHelper.addData(dbData7);
         }
+
     }
-
-
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return PlaceholderFragment.newInstance(position + 1);
-
-        }
-
-        @Override
-        public int getCount() {
-            return 2;
-        }
-
-            @Override
-            public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return getString(R.string.Log);
-                case 1:
-                    return getString(R.string.Data);
-            }
-            return null;
-        }
-    }
-         public void showPopup(View view){
-            PopupMenu popupMenu = new PopupMenu(this, view);
-            MenuInflater inflater = popupMenu.getMenuInflater();
-            inflater.inflate(R.menu.duration, popupMenu.getMenu());
-            popupMenu.show();
-        }
-
 }
