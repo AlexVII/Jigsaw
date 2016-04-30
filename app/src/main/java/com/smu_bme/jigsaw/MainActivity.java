1ba6a54501df62f5e469618b70b13738ed14b6d5
bme-lab2 <bme@lab2.com>
2016-04-30 14:13:29 +0800

    Added Selected Menu

--- app/src/main/java/com/smu_bme/jigsaw/MainActivity.java
+++ app/src/main/java/com/smu_bme/jigsaw/MainActivity.java
@@ -1,6 +1,7 @@
 package com.smu_bme.jigsaw;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -16,7 +17,9 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
@@ -61,6 +64,7 @@ public class MainActivity extends AppCompatActivity{
 
     public static final String CurrentDateString =  new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
     public static final String ShowedDateString = CurrentDateString;
+    public static final Calendar ShowedDate = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,34 +91,85 @@ public class MainActivity extends AppCompatActivity{
                 final View layout = inflater.inflate(R.layout.dialog, null);
                 final EditText name = (EditText) layout.findViewById(R.id.create_name);
                 final EditText remark = (EditText) layout.findViewById(R.id.create_remark);
+                final TextView textView = (TextView) layout.findViewById(R.id.edit_duration);
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showPopupMenu(MainActivity.this, v);
+                    }
+                });
                 new AlertDialog.Builder(MainActivity.this).setTitle(getString(R.string.create)).setView(layout).setPositiveButton(getString(R.string.yes),
                         new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 String nameInput = name.getText().toString();
                                 String remarkInput = remark.getText().toString();
-
-                                if (nameInput.equals("")){
-                                    Toast.makeText(MainActivity.this,getString(R.string.noName), Toast.LENGTH_SHORT).show();
-                                } else if (remarkInput.equals("")) {
-                                    Toast.makeText(MainActivity.this, getString(R.string.noRemark), Toast.LENGTH_SHORT).show();
+                                Calendar calendar = Calendar.getInstance();
+                                if (nameInput.equals("")) {
+                                    Toast.makeText(MainActivity.this, getString(R.string.noName), Toast.LENGTH_SHORT).show();
                                 } else {
-                                    DbData dbData = new DbData(CurrentDateString,   );
+                                    int duration = 900;
+                                    switch (checkedItemId){
+                                        case R.id.quart_hour: duration = 900; break;
+                                        case R.id.half_hour: duration = 1800; break;
+                                        case R.id.one_hour: duration = 3600; break;
+                                        case R.id.one_and_half_hour: duration = 5400; break;
+                                        case R.id.two_hour: duration = 7200; break;
+                                        case R.id.three_hour: duration = 10800; break;
+                                    }
+                                    Toast.makeText(MainActivity.this, "Duration is" + duration, Toast.LENGTH_SHORT).show();
+                                    if (remarkInput.equals("")){
+//                                        DbData dbData = new DbData(CurrentDateString, calendar.get(Calendar.HOUR) + calendar.get(Calendar.MINUTE) + "",duration, nameInput);
+                                    } else {
+//                                        DbData dbData = new DbData(CurrentDateString, calendar.get(Calendar.HOUR) + calendar.get(Calendar.MINUTE) + "",duration, nameInput, remarkInput );
+                                    }
                                 }
                             }
                         }).setNegativeButton(getString(R.string.cancel), null).show();
             }
         });
+    }
 
-}
-
+    private int checkedItemId = R.id.quart_hour;
+    private void showPopupMenu(final Context context, View view){
+        PopupMenu popupMenu = new PopupMenu(context, view);
+        popupMenu.inflate(R.menu.duration);
+        popupMenu.getMenu().findItem(checkedItemId).setChecked(true);
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.quart_hour:
+                        checkedItemId = R.id.quart_hour;
+                        break;
+                    case R.id.half_hour:
+                        checkedItemId = R.id.half_hour;
+                        break;
+                    case R.id.one_hour:
+                        checkedItemId = R.id.one_hour;
+                        break;
+                    case R.id.one_and_half_hour:
+                        checkedItemId = R.id.one_and_half_hour;
+                        break;
+                    case R.id.two_hour:
+                        checkedItemId = R.id.two_hour;
+                        break;
+                    case R.id.three_hour:
+                        checkedItemId = R.id.three_hour;
+                        break;
+                }
+                return true;
+            }
+        });
+        popupMenu.show();
+    }
     public static class PlaceholderFragment extends Fragment {
 
-        String CurrentCalendar =  MainActivity.CurrentCalendar;
-        String ShowedCalendar = CurrentCalendar;
         Calendar calendar;
         int CurrentMins  = Calendar.getInstance().get(Calendar.MINUTE);
         private List<DbData> list;
+
+        private DbHelper dbHelper = new DbHelper(getActivity());
 
         private static final String ARG_SECTION_NUMBER = "section_number";
 
@@ -143,7 +198,7 @@ public class MainActivity extends AppCompatActivity{
         public View initCardAndProgressBar(LayoutInflater inflater, final ViewGroup container){
             View rootView = inflater.inflate(R.layout.layout_log, container, false);
             final TextView textView = (TextView) rootView.findViewById(R.id.date);
-            textView.setText(ShowedCalendar);
+            textView.setText(MainActivity.ShowedDateString);
             ImageButton imageButton = (ImageButton) rootView.findViewById(R.id.edit_date);
             imageButton.setOnClickListener(new View.OnClickListener() {
                @Override
@@ -165,11 +220,11 @@ public class MainActivity extends AppCompatActivity{
            });
             ProgressBar progressBar = (ProgressBar) rootView.findViewById(R.id.progress);
             progressBar.setMax(14400);
-            if (ShowedCalendar == CurrentCalendar){
+            if (ShowedDateString.equals(CurrentDateString)){
             progressBar.setSecondaryProgress(CurrentMins);
             } else { progressBar.setSecondaryProgress(1440);
-        }
-//            progressBar.setProgress();
+//   TODO         progressBar.setProgress();
+            }
             RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.event_list);
             recyclerView.setHasFixedSize(true);
             LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
@@ -329,11 +384,4 @@ public class MainActivity extends AppCompatActivity{
             return null;
         }
     }
-         public void showPopup(View view){
-            PopupMenu popupMenu = new PopupMenu(this, view);
-            MenuInflater inflater = popupMenu.getMenuInflater();
-            inflater.inflate(R.menu.duration, popupMenu.getMenu());
-            popupMenu.show();
-        }
-
 }
